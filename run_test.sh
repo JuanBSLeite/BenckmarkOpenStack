babf6e8d3c429d7ebacbbb6758b52a719fe7c94b
Juan Leite <juanbsleite@cbpf.br>
2026-03-24 17:51:56 -0300

    fix script

--- run_test.sh
+++ run_test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Create conda environment from conda.yaml
-mamba env create -f conda.yaml
+mamba env create -f conda.yaml -y
 
 # Activate the environment
 conda activate laura-env
 
 # compile Laura
-cd laura
+cd Laura++
 mkdir build
 cd build
 cmake .. -DLAURA_BUILD_ROOFIT_TASK=ON -DLAURA_BUILD_EXAMPLES=ON -DLAURA_BUILD_TESTS=ON
@@ -15,6 +15,11 @@ make -j$(nproc)
 
 # Run tests with timing
 
+localtime=$(date +"%Y-%m-%d %H:%M:%S")
+echo "Starting tests at: $localtime"
 
-time /home/almalinux/Laura++/build/examples/GenFit3pi gen 100
-time /home/almalinux/Laura++/build/examples/GenFit3pi fit 0 100
+time ./examples/GenFit3pi gen 100
+time ./examples/GenFit3pi fit 0 100
+
+endtime=$(date +"%Y-%m-%d %H:%M:%S")
+echo "Finished tests at: $endtime"
